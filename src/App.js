8a8c0478d39ad8910562eeecca788197e93f7a8d
thatsaphon <71420827+thatsaphon@users.noreply.github.com>
2021-03-17 15:54:01 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -14,106 +14,106 @@ function App() {
     getDate()
   }, [])
   return (
-    // <div
-    //   style={{
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     alignContent: "center",
-    //     // justifyContent: "center",
-    //     // textAlign: "center",
-    //     flexWrap: "wrap",
-    //   }}
-    // >
-    //   {/* <div style={{ display: "flex" }}> */}
-    //   <div
-    //     style={{
-    //       width: "90%",
-    //       height: "120px",
-    //       backgroundColor: "darkorange",
-    //       margin: "0 40px",
-    //     }}
-    //   ></div>
-    //   <div
-    //     style={{
-    //       width: "90%",
-    //       height: "40px",
-    //       backgroundColor: "darkblue",
-    //       margin: "0 40px",
-    //     }}
-    //   ></div>
-    //   <div
-    //     style={{
-    //       // margin: "0 5%",
-    //       width: "90%",
-    //       padding: "50px",
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       justifyContent: "center",
-    //       backgroundColor: "green",
-    //       flexWrap: "wrap",
-    //     }}
-    //   >
-    //     <div
-    //       style={{
-    //         width: "75%",
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignContent: "center",
-    //         backgroundColor: "rebeccapurple",
-    //       }}
-    //     >
-    //       {/* <div style={{ display: "flex", justifyContent: "center" }}> */}
-    //       <ul>
-    //         <li>Lorem ipsum dolor sit amet.</li>
-    //         <li>Lorem ipsum dolor sit amet.</li>
-    //         <li>Lorem ipsum dolor sit amet.</li>
-    //       </ul>
-    //       <img src="img/Slide1.JPG" alt="" />
-    //       {/* </div> */}
-    //     </div>
-    //   </div>
-    //   {/* </div> */}
-    // </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        // justifyContent: "center",
+        // textAlign: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      {/* <div style={{ display: "flex" }}> */}
+      <div
+        style={{
+          width: "90%",
+          height: "120px",
+          backgroundColor: "darkorange",
+          margin: "0 40px",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "90%",
+          height: "40px",
+          backgroundColor: "darkblue",
+          margin: "0 40px",
+        }}
+      ></div>
+      <div
+        style={{
+          // margin: "0 5%",
+          width: "90%",
+          padding: "50px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          backgroundColor: "green",
+          flexWrap: "wrap",
+        }}
+      >
+        <div
+          style={{
+            width: "75%",
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center",
+            backgroundColor: "rebeccapurple",
+          }}
+        >
+          {/* <div style={{ display: "flex", justifyContent: "center" }}> */}
+          <ul>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+          </ul>
+          <img src="img/Slide1.JPG" alt="" />
+          {/* </div> */}
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
 
-    <main>
-      <div> Add some text</div>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{" "}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{" "}
-        was bootstrapped with{" "}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{" "}
-        and contains three directories, <code>/public</code> for static assets,{" "}
-        <code>/src</code> for components and content, and <code>/api</code>{" "}
-        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
-        function. See{" "}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : "Loading date..."}</p>
-    </main>
+    // <main>
+    //   <div> Add some text</div>
+    //   <h1>Create React App + Go API</h1>
+    //   <h2>
+    //     Deployed with{" "}
+    //     <a
+    //       href="https://vercel.com/docs"
+    //       target="_blank"
+    //       rel="noreferrer noopener"
+    //     >
+    //       Vercel
+    //     </a>
+    //     !
+    //   </h2>
+    //   <p>
+    //     <a
+    //       href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
+    //       target="_blank"
+    //       rel="noreferrer noopener"
+    //     >
+    //       This project
+    //     </a>{" "}
+    //     was bootstrapped with{" "}
+    //     <a href="https://facebook.github.io/create-react-app/">
+    //       Create React App
+    //     </a>{" "}
+    //     and contains three directories, <code>/public</code> for static assets,{" "}
+    //     <code>/src</code> for components and content, and <code>/api</code>{" "}
+    //     which contains a serverless <a href="https://golang.org/">Go</a>{" "}
+    //     function. See{" "}
+    //     <a href="/api/date">
+    //       <code>api/date</code> for the Date API with Go
+    //     </a>
+    //     .
+    //   </p>
+    //   <br />
+    //   <h2>The date according to Go is:</h2>
+    //   <p>{date ? date : "Loading date..."}</p>
+    // </main>
   )
 }
 
